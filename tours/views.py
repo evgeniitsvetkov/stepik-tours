e52db4469924223abeedde37a05f588df2c2556e
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2020-05-01 18:46:35 +0300

    Update views functions, add data from data.py into templates

--- tours/views.py
+++ tours/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponseNotFound
+from random import sample
+
+from django.http import HttpResponseNotFound, Http404
 from django.shortcuts import render
 from django.views import View
 
@@ -7,17 +9,40 @@ import tours.data as data
 
 class MainView(View):
     def get(self, request):
-        return render(request, 'tours/index.html', context={'title': data.title})
+        sample_tours = sample(data.tours.items(), 6)
+
+        return render(request, 'tours/index.html', {'title': data.title,
+                                                    'subtitle': data.subtitle,
+                                                    'description': data.description,
+                                                    'departures': data.departures,
+                                                    'sample_tours': sample_tours})
 
 
 class DepartureView(View):
     def get(self, request, departure):
-        return render(request, 'tours/departure.html')
+        if departure not in data.departures:
+            raise Http404
+
+        tours_from_departure = {}
+        for tour_id, tour in data.tours.items():
+            if tour['departure'] == departure:
+                tours_from_departure[tour_id] = tour
+
+        return render(request, 'tours/departure.html', {'title': data.title,
+                                                        'departures': data.departures,
+                                                        'departure': departure,
+                                                        'tours_from_departure': tours_from_departure})
 
 
 class TourView(View):
     def get(self, request, tour_id):
-        return render(request, 'tours/tour.html')
+        if tour_id not in data.tours:
+            raise Http404
+
+        tour_data = data.tours[tour_id]
+        return render(request, 'tours/tour.html', {'title': data.title,
+                                                   'departures': data.departures,
+                                                   'tour': tour_data})
 
 
 def custom_handler404(request, exception):
